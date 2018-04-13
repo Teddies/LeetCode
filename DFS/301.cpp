86f9da94bd28ce080781683bb8403b42a5dee833
Teddies <Teddies@sjtu.edu.cn>
2018-04-13 00:06:54 -0700

    DFS

--- DFS/301.cpp
+++ DFS/301.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+	// First step: Count the numbers of invalid parentheses of left and right each
     void count(string s, int &left, int &right) {
         int l = 0, r = 0;
         left = 0, right = 0;
@@ -18,12 +19,15 @@ public:
         left = l - r;
     }
     
+    // Second step: Write a function to judge whether the string is valid
+    // Notice there might be letters within the string
     bool isValid(string s) {
         stack<char> st;
         for (char c : s) {
             if (c == '(') {
                 st.push('(');
             }
+            // Cannot use else because there might have letters
             if (c == ')') {
                 if (st.empty()) 
                     return false;
@@ -34,18 +38,28 @@ public:
         return st.empty();
     }
     
+    // The left and right values are used for the recursion
+    // The start value is used to record where to start within the string
+    // The character before the start value (start index in the vector)
+    // has already been discussed in the previous cases, so during the 
+    // recursion we always discuss the characters after the start index
     vector<string> DFS(string s, int left, int right, int start) {
         vector<string> res;
+        // Actually this is not needed, but write here to make it clear
         if (s.empty()) {
             res.push_back("");
             return res;
         }
+        // Recursion basic condition (also the terminate condition)
         if (left == 0 && right == 0 && isValid(s)) {
             res.push_back(s);
             return res;
         }
         for (int i = start; i < s.size(); ++ i) {
             vector<string> v;
+            // Avoid the repeatitive results
+            // Reduce the branches of the tree structure to improve performance
+            // Continuous same parentheses may cause repeatitive results
             if (i > 0 && s[i - 1] == s[i])
                 continue;
             if (left > 0 && s[i] == '(') {
