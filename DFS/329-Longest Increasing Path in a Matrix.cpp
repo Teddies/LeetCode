3f6ab37e001249582e51a243f6714b3eacd1423f
Teddies <Teddies@sjtu.edu.cn>
2018-04-15 17:27:44 -0700

    Apr. 15

--- DFS/329-Longest Increasing Path in a Matrix.cpp
+++ DFS/329-Longest Increasing Path in a Matrix.cpp
@@ -1,11 +1,12 @@
-// Ordinary DFS with some cut of branches
-// Time Limit Exceed
-// Should be optimized with DP
+// Simple DFS
+// Trick here is to use a mark matrix to record the result of the visited nodes
+// Thus these nodes will not be DFS again later and will directly return the result
+// The mark matrix is kind of like a cache to speed up the performance
 class Solution {
 public:
     int DFS(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& mark) {
         int n1 = 1, n2 = 1, n3 = 1, n4 = 1;
-        mark[i][j] = 1;
+        if (mark[i][j] != -1) return mark[i][j];
         if (i - 1 >= 0 && matrix[i - 1][j] < matrix[i][j]) {
             n1 += DFS(matrix, i - 1, j, mark);
         }
@@ -18,6 +19,7 @@ public:
         if (j + 1 < matrix[i].size() && matrix[i][j + 1] < matrix[i][j]) {
             n4 += DFS(matrix, i, j + 1, mark);
         }
+        mark[i][j] = max(max(n1, n2), max(n3, n4));
         return max(max(n1, n2), max(n3, n4));
     }
     
@@ -26,12 +28,12 @@ public:
         vector<vector<int>> mark = matrix;
         for (int i = 0; i < matrix.size(); ++ i) {
             for (int j = 0; j < matrix[0].size(); ++ j) {
-                mark[i][j] = 0;
+                mark[i][j] = -1;
             }
         }
         for (int i = 0; i < matrix.size(); ++ i) {
             for (int j = 0; j < matrix[0].size(); ++ j) {
-                if (mark[i][j] == 1)
+                if (mark[i][j] != -1)
                     continue;
                 int n = DFS(matrix, i, j, mark);
                 max = n > max ? n : max;
